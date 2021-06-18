bc1a5bca9b42f2148663ec097cfc0820c73dd49c
Simon Taddiken <simon.taddiken@gmail.com>
2021-06-18 13:44:27 +0200

    Simplify example

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,25 +1,14 @@
 package com.example.demo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Autowired
-    private TestDocumentRepository repository;
-
     @Test
-    void testFindByName() {
-        repository.save(new TestDocument("Simon", "test"));
-        repository.save(new TestDocument("Simon", null));
-        final List<TestDocumentProjection> findByName = repository.findByName("Simon");
-        assertThat(findByName).hasSize(2);
+    void testContextStarts() {
+
     }
 
 }
